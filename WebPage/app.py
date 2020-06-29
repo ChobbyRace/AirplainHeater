c6f52e1c38fdb6a0694e3832573504a2e203f912
ChobbyRace <RobC697@gmail.com>
2020-06-29 08:33:20 -0400

    added server code, adjusted file setup

---
+++ WebPage/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
+@app.route('/turn_on/', methods=['POST'])
+def turn_on():
+    time = int(request.form.get('number', 0))
+    data = {'time': time}
+    data = jsonify(data)
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
