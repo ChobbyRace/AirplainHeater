b9490f060f5b5106349b3fda0ee2c8891b55506b
Robert Chase <60945670+RobC697@users.noreply.github.com>
2020-07-17 18:14:36 -0400

    fixed heater .php to have same format as other pages

--- templates/heater.php
+++ templates/heater.php
@@ -1,35 +1,37 @@
 <html>
 <head>
-    <link rel="stylesheet" href="../static/styles.css" />
+    <link rel="stylesheet" href="static/styles.css" />
+    <link rel="shortcut icon" href="#" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
+
 <body>
-<div id="p00">
-        <h1>Heater Control Page</h1>
-        <p><i>Control a heater from the conveinience of your own home!</i></p>
-</div>
-<div class="topnav">
-        <a href="../index.html">Home</a>
-        <a class="active" href="heater.php">Heater</a>
-        <a href="blog.html">Blog</a>
-        <a href="about.html">About</a>
-</div>
-<div>
-    <p> The ability to control the heater from your home is very cool.
-        This webpage has been a good learning experience
-        Make this into something cooler when you have the time
-    </p>
-    <br>
-    <!-- <div><p><?php include('myFile.txt'); ?></p></div> -->
-    <br>
+    <div id="header">
+            <h1>Heater Control Page</h1>
+            <p><i>Control a heater from the conveinience of your own home!</i></p>
+    </div>
+    <div class="topnav">
+            <a href="../index.html">Home</a>
+            <a class="active" href="heater.php">Heater</a>
+            <a href="blog.html">Blog</a>
+            <a href="about.html">About</a>
+    </div>
+    <div>
         <h2 id="p02">What would you like to do?</h2>
-        <form method="post">
-            <br>
-            &nbsp;  &nbsp; <input type="submit" value="ON" name="ON">
-            <br>
-            <br>
-            <br>
-            &nbsp;  &nbsp; <input type="submit" value="OFF" name="OFF">
-</div>
+        <p> The ability to control the heater from your home is very cool.
+            This webpage has been a good learning experience
+            Make this into something cooler when you have the time
+        </p>
+            <!-- <div><p><?php include('myFile.txt'); ?></p></div> -->
+            <h2 id="p02">What would you like to do?</h2>
+            <form method="post">
+                <br>
+                &nbsp;  &nbsp; <input type="submit" value="ON" name="ON">
+                <br>
+                <br>
+                <br>
+                &nbsp;  &nbsp; <input type="submit" value="OFF" name="OFF">
+    </div>
 </body>
 </html>
 
