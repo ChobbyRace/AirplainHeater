73598732bbd512e5f87a602a33b572ba5990c7e5
Robert Chase <60945670+RobC697@users.noreply.github.com>
2020-07-24 20:28:33 -0400

    trying word doc

--- articles/template.php
+++ articles/template.php
@@ -19,7 +19,7 @@
         </div>
         
         <?php
-        $myfile = fopen("article/file.txt", "r") or die("Unable to open file!");
-        echo fread($myfile,filesize("article/file.txt"));
+        $myfile = fopen("We Like To Fly.docx", "r") or die("Unable to open file!");
+        echo fread($myfile,filesize("We Like To Fly.docx"));
         fclose($myfile);
         ?>
