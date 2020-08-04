1c48ade34053b4590736d7e8f7f7cca4b0cf20f5
Robert Chase <60945670+RobC697@users.noreply.github.com>
2020-08-04 14:59:00 -0400

    again trying

--- templates/heater.php
+++ templates/heater.php
@@ -24,16 +24,35 @@
         <h3 id="p03"> The heater is currently : </h3>
        
        <?php
-        $fp = fopen("../logs/heater_status.txt");
-        fseek($fp, -1, SEEK_END); 
-        $pos = ftell($fp);
-        $LastLine = "";
-        // Loop backword util "\n" is found.
-        while((($C = fgetc($fp)) != "\n") && ($pos > 0)) {
-            $LastLine = $C.$LastLine;
-            fseek($fp, $pos--);
-        }
-        echo $LastLine;
+$line = '';
+
+$f = fopen('../logs/heater_status.txt', 'r');
+$cursor = -1;
+
+fseek($f, $cursor, SEEK_END);
+$char = fgetc($f);
+
+/**
+ * Trim trailing newline chars of the file
+ */
+while ($char === "\n" || $char === "\r") {
+    fseek($f, $cursor--, SEEK_END);
+    $char = fgetc($f);
+}
+
+/**
+ * Read until the start of file or first newline char
+ */
+while ($char !== false && $char !== "\n" && $char !== "\r") {
+    /**
+     * Prepend the new char
+     */
+    $line = $char . $line;
+    fseek($f, $cursor--, SEEK_END);
+    $char = fgetc($f);
+}
+
+echo $line;
         ?>
             <!-- <div><p><?php include('myFile.txt'); ?></p></div> -->
             <form method="post" style="text-align: center;">
