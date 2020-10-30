f03694563437bd209f5ed531e1b71e18b8826c7d
ChobbyRace <34276524+ChobbyRace@users.noreply.github.com>
2020-10-30 16:59:53 -0400

    added temp sensor read
    added to read_temp.py to read the temperature for the actual sensor, not the cpu

--- PythonScripts/read_temp.py
+++ PythonScripts/read_temp.py
@@ -1,10 +1,32 @@
 #import Adafruit_DHT as dht    # Importing Adafruit library for DHT22
 from datetime import *
 from random import randrange
+import glob
 
-def get_temp():
-    #temp = dht.read_retry(dht.DHT22, dht22Pin)
-    temp = 69
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
+
+def write_temp():
+    temp = read_temp()
     temp = ('{0:0.1f}'.format(temp))
     with open("../cpu_temp.csv") as log:
         first_line = log.readline()
@@ -16,4 +38,4 @@ def get_temp():
         if first_line:
             log.write("{0},{1},{2}\n".format("Date","Time","Temp (C*)"))
         log.write("{0},{1},{2}\n".format(date.today(),datetime.now().strftime("%H:%M:%S"),str(temp)))
-get_temp()
+write_temp()
