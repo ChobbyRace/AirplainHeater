1b26957ed73ce088004b746e364a1673fc2297e5
ChobbyRace <34276524+ChobbyRace@users.noreply.github.com>
2020-11-01 18:44:41 -0500

    updated on and off scripts
    updated scripts with correct pin layout and high/low layout

--- PythonScripts/off.py
+++ PythonScripts/off.py
@@ -4,12 +4,12 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-channel = 21 #whatever you have it hooked up to
+channel = 32 #whatever you have it hooked up to
 state = GPIO.LOW
 GPIO.setup(channel, GPIO.OUT)
 #GPIO.output(channel, state)
 def heater_off(pin):
-    GPIO.output(pin, GPIO.LOW) #turn off heater
+    GPIO.output(pin, GPIO.HIGH) #turn off heater
     channel_status = GPIO.input(pin)
     print("Pin ",pin," Status: ",channel_status)
 
--- PythonScripts/on.py
+++ PythonScripts/on.py
@@ -4,12 +4,12 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-channel = 21 #whatever you have it hooked up to
+channel = 32 #whatever you have it hooked up to
 state = GPIO.LOW
 GPIO.setup(channel, GPIO.OUT)
 #GPIO.output(channel, state)  this is the general syntax
 def heater_on(pin):
-     GPIO.output(pin, GPIO.HIGH) #turn on heater
+     GPIO.output(pin, GPIO.LOW) #turn on heater
      channel_status = GPIO.input(pin)
      print("Pin ",pin," Status: ",channel_status)
 
