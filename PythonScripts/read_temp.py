531ec47905bbd0fb032d2b8701ff279817e541f3
ChobbyRace <RobC697@gmail.com>
2020-11-02 08:58:49 -0500

    fixed temp read fuction IOT work with cronjob

--- PythonScripts/read_temp.py
+++ PythonScripts/read_temp.py
@@ -12,11 +12,11 @@ def read_temp_raw():
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+        time.sleep(0.5)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
@@ -28,13 +28,13 @@ def read_temp():
 def write_temp():
     temp = read_temp()
     temp = ('{0:0.1f}'.format(temp))
-    with open("../cpu_temp.csv") as log:
+    with open("/var/www/html/AirplainHeater/cpu_temp.csv") as log:
         first_line = log.readline()
         if first_line == "":
             first_line = True
         else:
             first_line = False
-    with open("../cpu_temp.csv", 'a') as log:
+    with open("/var/www/html/AirplainHeater/cpu_temp.csv", 'a') as log:
         if first_line:
             log.write("{0},{1},{2}\n".format("Date","Time","Temp (C*)"))
         log.write("{0},{1},{2}\n".format(date.today(),datetime.now().strftime("%H:%M:%S"),str(temp)))
