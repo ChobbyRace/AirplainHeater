c0332b85448cc4db81af81d2a7c1c8dd53473e77
Robert Chase <60945670+RobC697@users.noreply.github.com>
2020-11-11 16:30:08 -0500

    Finalized File Structure, moved unused files to respective folders, edited home page to only have needed pages, changed home page text content, commented out unused java on home page, edited file location for temp reading python function

--- PythonScripts/read_temp.py
+++ PythonScripts/read_temp.py
@@ -28,13 +28,13 @@ def read_temp():
 def write_temp():
     temp = read_temp()
     temp = ('{0:0.1f}'.format(temp))
-    with open("/var/www/html/AirplainHeater/cpu_temp.csv") as log:
+    with open("/var/www/AirplainHeater/cpu_temp.csv") as log:
         first_line = log.readline()
         if first_line == "":
             first_line = True
         else:
             first_line = False
-    with open("/var/www/html/AirplainHeater/cpu_temp.csv", 'a') as log:
+    with open("/var/www/AirplainHeater/cpu_temp.csv", 'a') as log:
         if first_line:
             log.write("{0},{1},{2}\n".format("Date","Time","Temp (C*)"))
         log.write("{0},{1},{2}\n".format(date.today(),datetime.now().strftime("%H:%M:%S"),str(temp)))
