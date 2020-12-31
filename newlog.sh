bd462952708edeb36746ad81a9fba6c2055fb98a
ChobbyRace <34276524+ChobbyRace@users.noreply.github.com>
2020-12-31 18:31:45 -0500

    fixed newlog script to actually flush tmp log

--- newlog.sh
+++ newlog.sh
@@ -2,6 +2,6 @@
 #this will transfer the current cpu data to the log file with a new date
 #clears cpu_temp.csv for the next day's data
 now=$(date +"%Y-%m-%d");
-file_name="/var/www/html/AirplainHeater/logs/$now.cpu_temp.csv";
-cp /var/www/html/AirplainHeater/cpu_temp.csv $file_name;
-> /var/www/html/AirplainHeater/cpu_temp.csv;
+file_name="/var/www/AirplainHeater/logs/$now.cpu_temp.csv";
+cp /var/www/AirplainHeater/cpu_temp.csv $file_name;
+> /var/www/AirplainHeater/cpu_temp.csv;
